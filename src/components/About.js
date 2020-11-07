7d751e13ea1437dd7da92c0ad45414a860e680f8
vpham927 <pha927@gmail.com>
2020-11-07 17:26:13 +0000

    v1

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+render() {
+        return (
+        <section id='about'>
+            <body>
+            <h2>Viet Pham</h2>
+            <h4>git:</h4>
+            </body>
+        </section>
+        );
+    }
+}
