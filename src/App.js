742ea2374445a3b9296eb1d2e4b7dae949697487
vpham927 <pha927@gmail.com>
2020-11-07 17:45:59 +0000

    commiting changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
 import About from './components/About';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
+
 class App extends Component {
   render() {
     return (
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
 export default class About extends Component {
 render() {
         return (
         <section id='about'>
             <body>
             <h2>Viet Pham</h2>
-            <h4>git:</h4>
+            <h4>Git Icon - https://github.com/vpham927 </h4>
+            <h4>Linkedin Icon - https://github.com/vpham927 </h4>
             </body>
         </section>
         );
